ac11dc9e5c4b78a8a606e5f30c39e9d9214284b3
Alan Loh <yaluen@uw.edu>
2013-04-16 11:54:20 -0700

    More basic functionality: Added button for accessing server (no functionality yet)

--- Nonogram/src/uw/cse403/nonogramfun/MainActivity.java
+++ Nonogram/src/uw/cse403/nonogramfun/MainActivity.java
@@ -2,7 +2,9 @@ package uw.cse403.nonogramfun;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -18,6 +20,12 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+    
+    
+    /** Called when the user clicks the Access button */
+    public void accessServer(View view) {
+        // Do something in response to button
     }
     
 }
