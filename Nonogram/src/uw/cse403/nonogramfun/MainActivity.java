ae3a74e4c9c7eb5e5b3e9d8d7661cb01e2d39ae3
renhax <renhax@jamie.cs.washington.edu>
2013-04-18 14:40:12 -0700

    able to access the data

--- Nonogram/src/uw/cse403/nonogramfun/MainActivity.java
+++ Nonogram/src/uw/cse403/nonogramfun/MainActivity.java
@@ -1,17 +1,29 @@
 package uw.cse403.nonogramfun;
 
 import android.os.Bundle;
+
 import android.app.Activity;
-import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
+import android.widget.TextView;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class MainActivity extends Activity {
+	private TextView myText = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+ 
+        myText = new TextView(null);
+        
     }
 
 
@@ -23,9 +35,17 @@ public class MainActivity extends Activity {
     }
     
     
-    /** Called when the user clicks the Access button */
-    public void accessServer(View view) {
+    /** Called when the user clicks the Access button 
+     * @throws SQLException */
+    public void accessServer(View view) throws SQLException {
         // Do something in response to button
+    	Connection con = DriverManager.getConnection("jdbc:mysql://82.197.130.17", 
+    			"1361466_00e6", "uwcse403nanogram");
+    	PreparedStatement statement = con.prepareStatement("SELECT first_name FROM developers WHERE id = 1");
+    	ResultSet result = statement.executeQuery();
+    	while (result.next()) {
+    		
+    	}
     }
     
 }
