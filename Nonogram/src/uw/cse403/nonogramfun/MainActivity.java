a30e203bd82cd1dcb8d305b8b033d9be6fd8512f
renhax <renhax@jamie.cs.washington.edu>
2013-04-18 15:00:16 -0700

    fix some bugs

--- Nonogram/src/uw/cse403/nonogramfun/MainActivity.java
+++ Nonogram/src/uw/cse403/nonogramfun/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.sql.Connection;
@@ -14,15 +15,13 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class MainActivity extends Activity {
-	private TextView myText = null;
+	private EditText myText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
- 
-        myText = new TextView(null);
+        myText = (EditText) findViewById(R.id.editText1);
         
     }
 
@@ -37,15 +36,13 @@ public class MainActivity extends Activity {
     
     /** Called when the user clicks the Access button 
      * @throws SQLException */
-    public void accessServer(View view) throws SQLException {
+    private void accessServer() throws SQLException {
         // Do something in response to button
     	Connection con = DriverManager.getConnection("jdbc:mysql://82.197.130.17", 
     			"1361466_00e6", "uwcse403nanogram");
     	PreparedStatement statement = con.prepareStatement("SELECT first_name FROM developers WHERE id = 1");
     	ResultSet result = statement.executeQuery();
-    	while (result.next()) {
-    		
-    	}
+    	myText.setText(result.getString("1"));
     }
     
 }
