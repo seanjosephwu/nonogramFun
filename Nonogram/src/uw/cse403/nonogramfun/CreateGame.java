7a19d42546dddefbd5220a72ffb3e7d2001b4ce9
Renhao Xie <renhax@hare.cs.washington.edu>
2013-04-26 13:27:31 -0700

    add create_game.xml

---
+++ Nonogram/src/uw/cse403/nonogramfun/CreateGame.java
@@ -0,0 +1,22 @@
+package uw.cse403.nonogramfun;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+
+public class CreateGame extends Activity {
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.create_game);
+		Log.i("create a game", "on create");
+	}
+	
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+}
