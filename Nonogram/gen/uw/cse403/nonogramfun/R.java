5b2baa3dfef3ed699944c362a78520ebd8223586
renhax <renhax@hare.cs.washington.edu>
2013-04-26 13:16:00 -0700

    add a new screen to create a game

--- Nonogram/gen/uw/cse403/nonogramfun/R.java
+++ Nonogram/gen/uw/cse403/nonogramfun/R.java
@@ -25,12 +25,15 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
+        public static final int action_settings=0x7f080004;
         public static final int button1=0x7f080000;
+        public static final int creategame=0x7f080003;
         public static final int editText1=0x7f080001;
+        public static final int imageView1=0x7f080002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int create_game=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
