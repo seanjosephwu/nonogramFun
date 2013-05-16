8a4277f7918df7c0ed17b88b619cbbf9f929c4bd
Sean Wu <seanjosephwu@gmail.com>
2013-05-15 17:16:07 -0700

    Reorganized tests

---
+++ Nonogram/tests/src/uw/cse403/nonogramfun/network/Test_NonoNetwork.java
@@ -0,0 +1,14 @@
+package uw.cse403.nonogramfun.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Test_NonoNetwork {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ Nonogram/tests/src/uw/cse403/nonogramfun/utility/Test_NonoUtil.java
@@ -0,0 +1,14 @@
+package uw.cse403.nonogramfun.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Test_NonoUtil {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
