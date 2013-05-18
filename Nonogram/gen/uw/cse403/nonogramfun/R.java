284b282d1dedd556dd7a178a57a5c51673d15fc8
Alan Loh <yaluen@uw.edu>
2013-05-18 15:39:19 -0700

    cleaned up deleted activities and changed title text of activities to match their activities

--- Nonogram/gen/uw/cse403/nonogramfun/R.java
+++ Nonogram/gen/uw/cse403/nonogramfun/R.java
@@ -73,29 +73,26 @@ public final class R {
         public static final int access_database=0x7f050003;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int create_large_button=0x7f05000f;
-        public static final int create_medium_button=0x7f05000e;
-        public static final int create_small_button=0x7f05000d;
+        public static final int create_large_button=0x7f05000c;
+        public static final int create_medium_button=0x7f05000b;
+        public static final int create_small_button=0x7f05000a;
         public static final int creategame_button=0x7f050005;
         public static final int hello_world=0x7f050002;
         public static final int howtoplay_button=0x7f050006;
-        public static final int play_large_button=0x7f050012;
-        public static final int play_medium_button=0x7f050011;
-        public static final int play_small_button=0x7f050010;
+        public static final int play_large_button=0x7f05000f;
+        public static final int play_medium_button=0x7f05000e;
+        public static final int play_small_button=0x7f05000d;
         public static final int playgame_button=0x7f050007;
-        public static final int rankings_button=0x7f050009;
-        public static final int scoreboardLarge=0x7f05001a;
-        public static final int scoreboard_acknowledgement=0x7f050017;
-        public static final int scoreboardmedium=0x7f050019;
-        public static final int scoreboardsmall=0x7f050018;
+        public static final int scoreboardLarge=0x7f050017;
+        public static final int scoreboard_acknowledgement=0x7f050014;
+        public static final int scoreboard_button=0x7f050009;
+        public static final int scoreboardmedium=0x7f050016;
+        public static final int scoreboardsmall=0x7f050015;
         public static final int settings_button=0x7f050008;
-        public static final int title_activity_create_game_large=0x7f05000c;
-        public static final int title_activity_create_game_medium=0x7f05000b;
-        public static final int title_activity_create_game_menu=0x7f050013;
-        public static final int title_activity_create_game_screen=0x7f050014;
-        public static final int title_activity_create_game_small=0x7f05000a;
-        public static final int title_activity_play_game_menu=0x7f050015;
-        public static final int title_activity_play_game_screen=0x7f050016;
+        public static final int title_activity_create_game_menu=0x7f050010;
+        public static final int title_activity_create_game_screen=0x7f050011;
+        public static final int title_activity_play_game_menu=0x7f050012;
+        public static final int title_activity_play_game_screen=0x7f050013;
     }
     public static final class style {
         /** 
