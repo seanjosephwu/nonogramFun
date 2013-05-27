a110804593ad49f7ddcf6e34aa42dedc4569bb98
supershizhe <zheshi@uw.edu>
2013-05-27 17:13:05 -0600

    Update HowToPlay.java

--- Nonogram/src/uw/cse403/nonogramfun/HowToPlay.java
+++ Nonogram/src/uw/cse403/nonogramfun/HowToPlay.java
@@ -27,10 +27,10 @@ public class HowToPlay extends Activity {
 		setTitle("How To Play");
 	}
 	 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+        	// Inflate the menu; this adds items to the action bar if it is present.
+        	getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 	
