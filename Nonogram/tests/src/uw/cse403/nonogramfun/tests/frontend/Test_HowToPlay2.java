e21735c1c8a47e59839edafd2cb04756535cef95
Angel Wang <huiqiw@uw.edu>
2013-06-01 14:39:15 -0700

    comments added to test classes

---
+++ Nonogram/tests/src/uw/cse403/nonogramfun/tests/frontend/Test_HowToPlay2.java
@@ -0,0 +1,59 @@
+package uw.cse403.nonogramfun.tests.frontend;
+
+import com.jayway.android.robotium.solo.Solo;
+import uw.cse403.nonogramfun.*;
+import android.test.ActivityInstrumentationTestCase2;
+/**
+ * Black Box Tests
+ * HowToPlay2 class
+ * @author Huiqi Wang
+ *
+ */
+public class Test_HowToPlay2 extends ActivityInstrumentationTestCase2<HowToPlay2> {
+	private Solo solo;
+	
+	public Test_HowToPlay2() {
+		super(HowToPlay2.class);
+	}
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		solo = new Solo(getInstrumentation(),getActivity());
+	}
+
+	public void testViewsCreated(){
+		assertEquals(true, solo.searchButton("next"));
+		assertEquals(true, solo.searchButton("skip"));
+		assertEquals(true, solo.searchButton("prev"));
+	}
+	
+	public void testPrevButton(){
+		solo.assertCurrentActivity("Not HowToPlay2", HowToPlay2.class);
+		solo.clickOnButton("prev");
+		solo.assertCurrentActivity("Not HowToPlay", HowToPlay.class);
+	}
+	
+	public void testNextButton(){
+		solo.assertCurrentActivity("Not HowToPlay2", HowToPlay2.class);
+		solo.clickOnButton("next");
+		solo.assertCurrentActivity("Not HowToPlay3", HowToPlay3.class);
+		
+		// test going back to HowToPlay2 from HowToPlay3
+		assertEquals(true, solo.searchButton("prev"));
+		solo.clickOnButton("prev");
+		solo.assertCurrentActivity("Not HowToPlay2", HowToPlay2.class);
+	}
+	
+	public void testSkipButton(){
+		solo.assertCurrentActivity("Not HowToPlay2", HowToPlay2.class);
+		solo.clickOnButton("skip");
+		solo.assertCurrentActivity("Not MainAcitivity", MainActivity.class);
+	}
+	
+	@Override
+	protected void tearDown() throws Exception {
+		solo.finishOpenedActivities();
+	}
+
+}
---
+++ Nonogram/tests/src/uw/cse403/nonogramfun/tests/frontend/Test_HowToPlay3.java
@@ -0,0 +1,41 @@
+package uw.cse403.nonogramfun.tests.frontend;
+
+import com.jayway.android.robotium.solo.Solo;
+import uw.cse403.nonogramfun.*;
+import android.test.ActivityInstrumentationTestCase2;
+/**
+ * Black Box Tests
+ * HowToPlay3 class
+ * @author Huiqi Wang
+ *
+ */
+public class Test_HowToPlay3 extends ActivityInstrumentationTestCase2<HowToPlay3> {
+	private Solo solo;
+	
+	public Test_HowToPlay3() {
+		super(HowToPlay3.class);
+	}
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		solo = new Solo(getInstrumentation(),getActivity());
+	}
+
+	public void testViewsCreated(){
+		assertEquals(true, solo.searchButton("done"));
+		assertEquals(true, solo.searchButton("prev"));
+	}
+	
+	public void testDoneButton(){
+		solo.assertCurrentActivity("Not HowToPlay3", HowToPlay3.class);
+		solo.clickOnButton("done");
+		solo.assertCurrentActivity("Not MainAcitivity", MainActivity.class);
+	}
+	
+	@Override
+	protected void tearDown() throws Exception {
+		solo.finishOpenedActivities();
+	}
+
+}
